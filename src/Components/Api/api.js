3c4b4a0b5577bbd12d9061164dd3a7bc4edc45b7
thegetoutgirl <amyahaha@gmail.com>
2020-04-03 22:43:31 -0400

    it's alivegit add .git add .!

--- src/Components/Api/api.js
+++ src/Components/Api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// Calls 20 people in a list
+// Calls 100 people in a list
 export default {
     people: function() {
     return axios.get(`https://randomuser.me/api/?results=100&nat=us&inc=id,picture,name,phone,location,email,dob`)
