283c55bf372eefc1b6ec43b34d6a78649c8da69b
thegetoutgirl <amyahaha@gmail.com>
2020-04-03 22:00:04 -0400

    Updates

---
+++ precache-manifest.c9be940c23ea835f1e3db0d355ac261a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f00520333cdbbb48e73cc4f5e1e2ba0",
+    "url": "/employee-jawn/index.html"
+  },
+  {
+    "revision": "83cd26963f006d9100d9",
+    "url": "/employee-jawn/static/css/main.4c60af33.chunk.css"
+  },
+  {
+    "revision": "6a9042059d6452bdd47c",
+    "url": "/employee-jawn/static/js/2.ea07265e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-jawn/static/js/2.ea07265e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83cd26963f006d9100d9",
+    "url": "/employee-jawn/static/js/main.05bca9eb.chunk.js"
+  },
+  {
+    "revision": "65dc77df813caee98b6e",
+    "url": "/employee-jawn/static/js/runtime-main.970ce451.js"
+  }
+]);
